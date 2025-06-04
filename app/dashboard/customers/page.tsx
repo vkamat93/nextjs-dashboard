ebdbf736fd3fae49efb176aa5cb69496c62f23a9
vikrantkamatpartner <vikrant.kamat@partner.bmwgroup.com>
2025-06-04 09:30:30 +0530

    First commit after UI changes

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <p>
+        Customers Page
+    </p>
+  )
+}
+
+export default page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <p>
+        Dashboard Page
+    </p>
+  )
+}
+
+export default page
